fb41d15711a48e079e0aad40a8003d74d3705a17
Aidan <2313924278@qq.com>
2022-12-29 17:04:57 +0800

    增加调整窗口大小功能

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -178,5 +178,29 @@ namespace InvoiceAssistant.Properties {
                 this["InvoiceZeroTaxRateID"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1400")]
+        public double MainWindowWidth {
+            get {
+                return ((double)(this["MainWindowWidth"]));
+            }
+            set {
+                this["MainWindowWidth"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("800")]
+        public double MainWindowHeight {
+            get {
+                return ((double)(this["MainWindowHeight"]));
+            }
+            set {
+                this["MainWindowHeight"] = value;
+            }
+        }
     }
 }
